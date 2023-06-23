00c178f26e346734694eb425a0a407e1ea24dca3
rahul01-git <rahulneupane0090@gmail.com>
2023-06-23 17:11:16 +0545

    trusted by section design completed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import Featured from '../../components/featured/Featured'
+import Trusted from '../../components/trusted/Trusted'
 import './Home.scss'
 
 const Home = () => {
   return (
     <div className='home'>
       <Featured/>
+      <Trusted/>
     </div>
   )
 }
