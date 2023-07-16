00544489233dc970f9e717b42b970e4defd4f0ee
rahul01-git <rahulneupane0090@gmail.com>
2023-07-16 14:27:28 +0545

    view myGigs

--- client/src/pages/myGigs/MyGigs.jsx
+++ client/src/pages/myGigs/MyGigs.jsx
@@ -1,100 +1,81 @@
 import { Link } from "react-router-dom";
 import "./MyGigs.scss";
+import getCurrentUser from "../../utils/getCurrentUser";
+import newRequest from "../../utils/newRequest";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 const MyGigs = () => {
+  const currentUser = getCurrentUser();
+  const queryClient = useQueryClient();
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ["myGigs"],
+    queryFn: () =>
+      newRequest.get(`/gigs?userId=${currentUser.id}}`).then((res) => {
+        return res.data;
+      }),
+  });
+
+  const mutation = useMutation({
+    mutationFn: (id) => {
+      return newRequest.delete(`/gigs/${id}`);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries("myGigs");
+      setReviewError(null);
+    },
+  });
+  console.log(data);
+  const handleDelete = (id) => {
+    mutation.mutate(id);
+  };
+
   return (
     <div className="myGigs">
-      <div className="container">
-        <div className="title">
-          <h1>Gigs</h1>
-          <Link to="/add">
-            <button>Add New Gig</button>
-          </Link>
+      {isLoading ? (
+        "loading"
+      ) : error ? (
+        "something went wrong"
+      ) : (
+        <div className="container">
+          <div className="title">
+            <h1>Gigs</h1>
+            <Link to="/add">
+              <button>Add New Gig</button>
+            </Link>
+          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>Image</th>
+                <th>Title</th>
+                <th>Price</th>
+                <th>Sales</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((gig) => (
+                <tr key={gig.id}>
+                  <td>
+                    <img className="img" src={gig.cover} alt="" />
+                  </td>
+                  <td>{gig.title}</td>
+                  <td>{gig.price}</td>
+                  <td>{gig.sales}</td>
+                  <td>
+                    <img
+                      className="delete"
+                      src="/img/delete.png"
+                      alt=""
+                      onClick={() => handleDelete(gig.id)}
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-        <table>
-          <tr>
-            <th>Image</th>
-            <th>Title</th>
-            <th>Price</th>
-            <th>Sales</th>
-            <th>Action</th>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="img"
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-            </td>
-            <td>Gig1</td>
-            <td>00</td>
-            <td>123</td>
-            <td>
-              <img className="delete" src="/img/delete.png" alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="img"
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-            </td>
-            <td>Gig1</td>
-            <td>00</td>
-            <td>123</td>
-            <td>
-              <img className="delete" src="/img/delete.png" alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="img"
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-            </td>
-            <td>Gig1</td>
-            <td>00</td>
-            <td>123</td>
-            <td>
-              <img className="delete" src="/img/delete.png" alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="img"
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-            </td>
-            <td>Gig1</td>
-            <td>00</td>
-            <td>123</td>
-            <td>
-              <img className="delete" src="/img/delete.png" alt="" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <img
-                className="img"
-                src="https://images.pexels.com/photos/270408/pexels-photo-270408.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
-              />
-            </td>
-            <td>Gig1</td>
-            <td>00</td>
-            <td>123</td>
-            <td>
-              <img className="delete" src="/img/delete.png" alt="" />
-            </td>
-          </tr>
-        </table>
-      </div>
+      )}
     </div>
   );
 };
